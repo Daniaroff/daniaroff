d05cb3a8f7a8b70f66f5e90a898abb65e738e6cd
daniaroff <daniaroff1@gmail.com>
2023-06-08 11:09:07 +0500

    firt commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// // const result = confirm("Are you here?");
+
+// // if (result) {
+// //   console.log("TRUE");
+// // }
+
+// // if (!result) {
+// //   console.log("FALSE");
+// // }
+
+// const age = +prompt("Сколько вам лет?", "18");
+
+// console.log(age + 10);
+// // if ((age = Number)) {
+// //   console.log("welcome");
+// // }
+console.log(4 + "5");
